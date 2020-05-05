1917d15804ccd256d017ec8159c79548b016d3d0
Dennis Schroer <dev@dennisschroer.nl>
2020-05-05 12:33:16 +0200

    Improve api specification

--- src/main/java/nl/dennisschroer/energytracker/EnergyTrackerApplication.java
+++ src/main/java/nl/dennisschroer/energytracker/EnergyTrackerApplication.java
@@ -2,6 +2,7 @@ package nl.dennisschroer.energytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.RepresentationModel;
 
 @SpringBootApplication
 public class EnergyTrackerApplication {
