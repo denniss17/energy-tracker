7253df423b8a8c72d288024c0c59e78dcad5e139
Dennis Schroer <dev@dennisschroer.nl>
2020-05-10 20:55:00 +0200

    Add creationDate to entity

--- src/test/groovy/nl/dennisschroer/energytracker/factory/MeterReadingEntityFactory.groovy
+++ src/test/groovy/nl/dennisschroer/energytracker/factory/MeterReadingEntityFactory.groovy
@@ -4,16 +4,16 @@ import nl.dennisschroer.energytracker.entity.MeterReadingEntity
 import nl.topicus.overheid.javafactorybot.Factory
 import nl.topicus.overheid.javafactorybot.definition.Attribute
 
-import java.time.OffsetDateTime
 import java.util.concurrent.TimeUnit
 
 @Singleton
 class MeterReadingEntityFactory extends Factory<MeterReadingEntity> {
     Map<String, Attribute> attributes = [
-            electricityNormal: value { faker.random().nextInt(1000) },
-            electricityLow   : value { faker.random().nextInt(1000) },
-            gas              : value { faker.random().nextInt(1000) },
-            water            : value { faker.random().nextDouble() },
-            timestamp        : value { faker.date().past(10, TimeUnit.DAYS) }
+            electricityNormal: value { faker.random().nextDouble().round(3) },
+            electricityLow   : value { faker.random().nextDouble().round(3) },
+            gas              : value { faker.random().nextDouble().round(3) },
+            water            : value { faker.random().nextDouble().round(3) },
+            timestamp        : value { faker.date().past(10, TimeUnit.DAYS) },
+            creationDate     : value { faker.date().past(5, TimeUnit.DAYS) }
     ]
 }
